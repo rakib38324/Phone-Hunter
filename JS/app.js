1f3a70ab747a842d37783158a2df97d6997cf5f6
rakib38324 <rakib3832@gmail.com>
2022-08-30 04:32:25 -0500

    Third commit

--- JS/app.js
+++ JS/app.js
@@ -42,7 +42,9 @@ const displayPhone = (phones,dataLimit) => {
             <div class="card-body">
                     <h5 class="card-title">${phone.phone_name}</h5>
                     <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
+
+                    <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal"> Show Details </button>
+                </div>
         </div>
         
         `;
@@ -61,6 +63,15 @@ toggleSpinner(false)
 document.getElementById('btn-Search').addEventListener('click',function(){
     
     processesSearch(10);
+
+})
+
+//search input fild enter key handle
+
+document.getElementById('phoneSearchText').addEventListener('keypress',function(even){
+    if (even.key === 'Enter'){
+        processesSearch(10);
+    }
 })
 
 const toggleSpinner = isLoading => {
@@ -86,4 +97,36 @@ const processesSearch = (dataLimit) =>{
 document.getElementById('btn-show-all').addEventListener('click',function(){
     processesSearch();
 })
-// loadPhone();
+
+const loadPhoneDetails =  async id =>{
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhoneDetails(data.data)
+}
+
+const displayPhoneDetails = phone =>{
+    console.log(phone)
+    const modalTitle = document.getElementById('phoneDetailModalLabel');
+    modalTitle.innerText = phone.name;
+
+    const phoneDetails = document.getElementById('phone-details');
+    phoneDetails.innerHTML = `
+    
+    <p> <spam class = "fw-bold"> Release Date:</spam> ${phone.releaseDate ? phone.releaseDate : "No Release Date Found"} </p>
+    <p> <spam class = "fw-bold"> Storage: </spam> ${phone.mainFeatures ? phone.mainFeatures.storage : "No Storage information Found"} </p>
+    <p> <spam class = "fw-bold"> Display: </spam> ${phone.mainFeatures ? phone.mainFeatures.displaySize : "Display Information not Found"} </p>
+    <p> <spam class = "fw-bold"> ChipSet: </spam> ${phone.mainFeatures ? phone.mainFeatures.chipSet : "ChipSet Information not Found"} </p>
+    <p> <spam class = "fw-bold"> Bluetooth: </spam> ${phone.others ? phone.others.Bluetooth : "Bluetooth Informatio not Found"}</p>
+    <p> <spam class = "fw-bold"> GPS: </spam> ${phone.others ? phone.others.GPS : "GPS Informatio not Found"}</p>
+    <p> <spam class = "fw-bold"> NFC: </spam> ${phone.others ? phone.others.NFC : "NFC Informatio not Found"}</p>
+    <p> <spam class = "fw-bold"> Radio: </spam> ${phone.others ? phone.others.NFC : "Radio Informatio not Found"}</p>
+    <p> <spam class = "fw-bold"> USB: </spam> ${phone.others ? phone.others.USB : "USB Informatio not Found"}</p>
+    <p> <spam class = "fw-bold"> WLAN: </spam> ${phone.others ? phone.others.WLAN : "WLAN Informatio not Found"}</p>
+
+    `;
+
+
+    
+}
+loadPhone("phone");
